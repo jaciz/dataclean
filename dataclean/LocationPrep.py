846ed4e1a41348ab710799b70527b2e6a628366c
Jaci <jacquelinezhangg@gmail.com>
2023-07-10 13:11:46 -0700

    Added function for stripping special char

--- dataclean/LocationPrep.py
+++ dataclean/LocationPrep.py
@@ -12,4 +12,17 @@ def valid_zip_codes(df, col='ZIP', pattern='^[0-9]{5}(-[0-9]{4})?$'):
             yes_list.append(eachnumber)
         else:
             no_list.append(eachnumber)
-    return print(f"There are {len(no_list)} entries that don't match zip code format")
+    print(f"There are {len(no_list)} entries that don't match zip code format")
+    return no_list
+
+def strip_character(string: str):
+    """Strips special characters. Use it on columns by using the pandas apply function (e.g. loc['Street'].apply(strip_character))
+
+    Args:
+        dataCol (str): string with special characters you want removed
+
+    Returns:
+        str: string with removed special characters
+    """
+    r = re.compile(r'[^A-Za-z\s0-9]+')
+    return r.sub('', string)
