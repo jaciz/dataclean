eb791ca603a2a88972004a8cd4fc55371f20f3e0
Jaci <jacquelinezhangg@gmail.com>
2023-08-31 11:50:44 -0700

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='dataclean',
-    version='0.0.2', 
+    version='0.0.3', 
     description='For data quality checks, cleaning, and manipulation',
     url='https://github.com/jaciz/dataclean.git',
     author='Jaci',
