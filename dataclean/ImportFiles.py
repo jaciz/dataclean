1a920d4e3917c4f5940de18ea5c1614cb36d0f65
Jaci <jacquelinezhangg@gmail.com>
2023-08-31 11:24:06 -0700

    additional import functions

--- dataclean/ImportFiles.py
+++ dataclean/ImportFiles.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import glob
 import os
+import polars as pl
+import pandas as pl
+# Increase column width to see more of the data
+pl.Config.set_fmt_str_lengths(200)
 
 # File reading
-def bringing_in_multiple_csv(path: str, dtype=None, parse_dates=None, skiprows=None, sep=None, header='infer', file_extension='csv'):
+def read_multiple_txt(path: str, dtype=None, parse_dates=None, skiprows=None, sep=None, header='infer', file_extension='csv'):
     """Brings in all files in a directory and combines it into one dataframe
 
     Args:
@@ -24,7 +28,7 @@ def bringing_in_multiple_csv(path: str, dtype=None, parse_dates=None, skiprows=N
     # Concatenate all DataFrames
     return pd.concat(df_list, ignore_index=True)
 
-def bringing_in_multiple_csv_with_filename(path, dtype=None, parse_dates=None, skiprows=None, sep=None, header='infer', file_extension='csv'):
+def read_multiple_txt_with_filename(path, dtype=None, parse_dates=None, skiprows=None, sep=None, header='infer', file_extension='csv'):
     """Brings in all files in a directory and combines it into one dataframe with a column listing the file name
 
     Args:
@@ -46,3 +50,38 @@ def bringing_in_multiple_csv_with_filename(path, dtype=None, parse_dates=None, s
         df = pd.concat([df, subset])
     
     return df
+
+# Functions written in polars
+def read_multiple_txt_with_filename_polars(path, dtypes=None, parse_dates=False, skip_rows=0, sep=',', has_header=True, null_values=None, new_columns=None, file_extension='csv'):
+    df = pl.DataFrame().lazy()
+    for p in list(glob.glob(f'{path}/*.{file_extension}')):
+        subset = (
+            pl.read_csv(p, dtypes=dtypes, parse_dates=parse_dates, skip_rows=skip_rows, has_header=has_header, sep=sep, new_columns=new_columns, infer_schema_length=0, null_values=null_values)
+            .lazy()
+        ).with_column(pl.lit(os.path.basename(p)).alias('file'))
+        
+
+        df = pl.concat([df, subset], how='diagonal')
+    
+    return df.collect()
+
+def read_multiple_txt_polars(path: str, dtypes=None, sep=None, has_header=True, file_extension='csv', new_columns=None):
+    # Create a list of dataframes
+    df_list = [
+        pl.read_csv(file, dtypes=dtypes, sep=sep, has_header=has_header, new_columns=new_columns)
+        for file in glob.glob(f'{path}/*.{file_extension}')
+    ]
+
+    # Concatenate all DataFrames
+    return pl.concat(df_list, how='diagonal')
+
+
+def read_multiple_parquet_polars(path):
+    # Create a list of dataframes
+    df_list = [
+        pl.read_parquet(file)
+        for file in glob.glob(f'{path}/*.parquet')
+    ]
+
+    # Concatenate all DataFrames
+    return pl.concat(df_list, how='diagonal')
